fb0ea90d665cedaa5d5c6ec33d2f779495c33426
Armin Herling <armin.herling@gmail.com>
2020-04-30 19:16:58 +0200

    Generating new state

--- GameOfLife/GameBoard.cs
+++ GameOfLife/GameBoard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace GameOfLife
 {
@@ -8,11 +9,13 @@ namespace GameOfLife
         private readonly int width;
         private readonly int height;
         private Stack<CellState[,]> history = new Stack<CellState[,]>();
+        private List<Group> groups = new List<Group>();
 
         public GameBoard(int boardWidth, int boardHeight)
         {
             width = boardWidth;
             height = boardHeight;
+            GenerateGroups();
             PushRandomizedCells();
         }
 
@@ -32,8 +35,45 @@ namespace GameOfLife
             history.Push(cells);
         }
 
+        private void GenerateGroups()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    var yAbove = y == height - 1 ? 0 : y + 1;
+                    var yBelow = y == 0 ? height - 1 : y - 1;
+                    var xLeft = x == 0 ? width - 1 : x - 1;
+                    var xRight = x == width - 1 ? 0 : x + 1;
+
+                    var neighbors = new List<Point>();
+                    neighbors.Add(new Point(xLeft, yAbove));
+                    neighbors.Add(new Point(x, yAbove));
+                    neighbors.Add(new Point(xRight, yAbove));
+
+                    neighbors.Add(new Point(xLeft, y));
+                    var center = new Point(x, y);
+                    neighbors.Add(new Point(xRight, y));
+
+                    neighbors.Add(new Point(xLeft, yBelow));
+                    neighbors.Add(new Point(x, yBelow));
+                    neighbors.Add(new Point(xRight, yBelow));
+
+                    groups.Add(new Group(center, neighbors));
+                }
+            }
+        }
+
         public void Update()
         {
+            var currentState = history.Peek();
+            var newState = new CellState[width, height];
+
+            foreach(var group in groups)
+            {
+                newState[group.X, group.Y] = group.NextState(currentState);
+            }
+            history.Push(newState);
         }
 
         public void Draw(IRenderer renderer)
---
+++ GameOfLife/Group.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace GameOfLife
+{
+    internal class Group
+    {
+        private readonly Point center;
+        private readonly List<Point> neighbors;
+
+        internal int X => center.X;
+        internal int Y => center.Y;
+
+        internal Group(Point center, List<Point> neighbors)
+        {
+            this.center = center;
+            this.neighbors = neighbors;
+        }
+
+        internal CellState NextState(in CellState[,] cells)
+        {
+            int livingNeighbors = 0;
+            foreach(var neighbor in neighbors)
+            {
+                if (cells[neighbor.X, neighbor.Y] == CellState.Alive)
+                    livingNeighbors++;
+            }
+
+            var currentState = cells[X, Y];
+            if (currentState == CellState.Alive)
+            {
+                if (livingNeighbors == 2 || livingNeighbors == 3)
+                    return CellState.Alive;
+                else
+                    return CellState.Dead;
+            }
+            else
+            {
+                if (livingNeighbors == 3)
+                    return CellState.Alive;
+            }
+
+            return currentState;
+        }
+    }
+}
